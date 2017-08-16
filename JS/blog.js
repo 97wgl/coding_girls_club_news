2761c5adb6234405de4e3e219097a07fdc127a92
Your Name <you@example.com>
2017-08-16 17:51:48 +0800

    new

--- JS/blog.js
+++ JS/blog.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     let count;
     $.ajax({
         type:'Get',
@@ -9,17 +8,12 @@ $(document).ready(function() {
             count = data[0].all_blogs_count;
         }
     });
-
+});
 /**
  * Created by xiongshasha on 17-8-16.
  */
 $().ready(()=>{
     $.get("/latest_blog",function(res){
-        // $('#post-title1').html(res[0].blog_title);
-        // $('#post-title2').html(res[0].blog_title);
-        // $('#post-title3').html(res[0].blog_title);
-        // $('#post-title4').html(res[0].blog_title);
-        // $('#post-title5').html(res[0].blog_title);
         let ul = $('#recent-post');
         let html_str ='';
         for(let i = 0;i < res.length;i ++){
@@ -34,6 +28,5 @@ $().ready(()=>{
             </li>`;
         }
         ul.html(html_str);
-
-    });
+    })
 });
--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
+//*******************邓雍*************************
 $(document).ready(function(){
-
     newsListInfo();
     let count = 0;
    $.ajax({
@@ -27,69 +27,10 @@ $(document).ready(function(){
            });
        }
    });
+});
 
-})
-/*function diPlaySearchNews(news) {
-    let count = news.length;
-    let page = parseInt((count + 4) / 4);
-    disPlayPage(1,news);
-    $(function () {
-=======
-    $.ajax({
-        type:'GET',
-        url:'/all_news_count',
-        async:false,
-        success:function (data) {
-            count = data[0].all_news_count;
-        }
-    });
-
-    let page = parseInt((count+4)/4);
-    displaynews(1);
-<<<<<<< HEAD
-
-    //分页
-=======
-<<<<<<< HEAD
-    $(function() {
-
-/*function disPlayPage(n,news) {
-    let i = (n-1) * 4;
-    for (; i < (n-1) * 4 + 4; i++) {
-        let li = $('<li></li>');
-        let img = $('<img>');
-        let h3 = $('<h3></h3>');
-        let p = $('<p></p>');
-        let a = $('<a></a>');
-        img.attr('src', `${news[i].news_image}`);
-        a.attr('href', `http://localhost:3000/HTML/detail.html?id=${nesws[i].id}`);
-        h3.html(`${news[i].news_title}`);
-        p.html(`${news[i].news_content}`);
-        li.append(img);
-        li.append(h3);
-        li.append(p);
-        a.append(li);
-        $('#simplecontent').append(a);
-    }
-}*/
->>>>>>> master
-    $("#pagination").pagination({
-        currentPage: 1,
-        totalPage: page,
-        isShow: true,
-        count: 7,
-        homePageText: "首页",
-        endPageText: "尾页",
-        prevPageText: "上一页",
-        nextPageText: "下一页",
-        callback: function(current) {
-          displaynews(current);
-          $('#todayLocation').trigger('click');
-        }
-    });
-
-    //搜索按钮事件
-    $('#searchIcon').click(()=>{
+ //搜索按钮事件
+$('#searchIcon').click(()=>{
         $('#pagination').html('');
         $('#simplecontent').html('');
         $.get(`/search_news?keywords=${$('#searchText').val()}`,(news)=>{
@@ -111,13 +52,6 @@ $(document).ready(function(){
             }
         })
     });
-
-//阻止页面刷新
-$().ready(()=>{
-    $('#searchForm').on('submit',(event)=>{
-        event.preventDefault();
-    })
-});
 // 轮播图以及旁边新闻列表
 function newsListInfo() {
     $.get('/news_list',(headlineNews)=> {
@@ -141,7 +75,7 @@ function newsListInfo() {
         }
     });
 }
-// 主要的新闻部分
+// 主要的新闻部分 dy不要改
 function displaynews(index) {
     $('#simplecontent').html('');
     $.get(`/all_news_pading?page=${index}`,(news)=>{
@@ -166,27 +100,36 @@ function displaynews(index) {
         }
     })
 }
+//*******************邓雍*************************
 
-function  displaySearch(nesws) {
-    for(i=0;i<nesws.length;i++){
+// ***********************俊俊******************************
+function displaySearch(news) {
+    $('#pagination').html('');
+    for (let i = 0; i < news.length; i++) {
         let li = $('<li></li>');
         let img = $('<img>');
         let h3 = $('<h3></h3>');
         let p = $('<p></p>');
-        img.attr('src',`${nesws[i].news_image}`);
-        h3.html(`${nesws[i].news_title}`);
-        p.html(`${nesws[i].news_content}`);
+        let a = $('<a></a>');
+        let span = $('<span></span>');
+        img.attr('src', `${news[i].news_image}`);
+        a.attr('href', `http://localhost:3000/HTML/detail.html?id=${news[i].id}`);
+        h3.html(`${news[i].news_title}`);
+        p.html(`${news[i].news_content}`);
+        span.html(`${news[i].news_time}`);
         li.append(img);
         li.append(h3);
         li.append(p);
-        $('#simplecontent').append(li);
-    }
+        li.append(span);
+        a.append(li);
+        $('#simplecontent').append(a);
 
+    }
 }
 $("#201701").click(function () {
     let id="2017-01";
     $.get(`/time_search?searchDate=${id}`,function (res) {
-       displaySearch(res);
+        displaySearch(res);
     });
     document.getElementById('pagination').totalPage=6;
 });
@@ -222,4 +165,4 @@ $("#201709").click(function () {
     });
 
 });
-
+// ***********************俊俊******************************
