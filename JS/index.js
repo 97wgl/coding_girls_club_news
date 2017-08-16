31531cff08c1a59498957e5b09a504b6f4335349
Your Name <you@example.com>
2017-08-16 14:01:53 +0800

    搜索

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    
+
     newsListInfo();
     let count = 0;
     $.ajax({
@@ -23,67 +23,39 @@ $(document).ready(function(){
         prevPageText: "上一页",
         nextPageText: "下一页",
         callback: function(current) {
-            alert('fdhsjfd')
           displaynews(current);
         }
     });
     //分页
     $('#searchSubmit').click(()=>{
-        $('#simplecontent').html('');
         $('#pagination').html('');
-        let SearchNews = $('#searchText').val();
-        $.get(`/search_news?keywords=${SearchNews}`,(afterSearchNews)=>{
-            let count = afterSearchNews.length;
-            page = parseInt((count+4)/4);
-            $("#pagination").pagination({
-                currentPage: 1,
-                totalPage: page,
-                isShow: true,
-                count: 7,
-                homePageText: "首页",
-                endPageText: "尾页",
-                prevPageText: "上一页",
-                nextPageText: "下一页",
-                callback: function(current) {
-                    alert(current);
-                    $('#simplecontent').html('');
-                    let end = 0;
-                    let start = (current-1)*4-1;
-                    if(current==page){
-                        end = afterSearchNews.length;
-                    } else{
-                        end = current*4
-                    }
-                    for(i=start;i<end;i++){
-                        let li = $('<li></li>');
-                        let img = $('<img>');
-                        let h3 = $('<h3></h3>');
-                        let p = $('<p></p>');
-                        let a = $('<a></a>');
-                        img.attr('src',`${afterSearchNews[i].news_image}`);
-                        a.attr('href',`http://localhost:3000/HTML/detail.html?id=${afterSearchNews[i].id}`);
-                        h3.html(`${afterSearchNews[i].news_title}`);
-                        p.html(`${afterSearchNews[i].news_content}`);
-                        li.append(img);
-                        li.append(h3);
-                        li.append(p);
-                        a.append(li);
-                        $('#simplecontent').append(a);
-                    }
-                }
-            });
-        });
+        $('#simplecontent').html('');
+        $.get(`/search_news?keywords=${$('#searchText').val()}`,(news)=>{
+            for(i=0;i<news.length;i++){
+                let li = $('<li></li>');
+                let img = $('<img>');
+                let h3 = $('<h3></h3>');
+                let p = $('<p></p>');
+                let a = $('<a></a>');
+                img.attr('src',`${news[i].news_image}`);
+                a.attr('href',`http://localhost:3000/HTML/detail.html?id=${news[i].id}`);
+                h3.html(`${news[i].news_title}`);
+                p.html(`${news[i].news_content}`);
+                li.append(img);
+                li.append(h3);
+                li.append(p);
+                a.append(li);
+                $('#simplecontent').append(a);
+            }
+        })
     })
 });
-
-
 
 $().ready(()=>{
     $('#searchForm').on('submit',(event)=>{
         event.preventDefault();
     })
 });
-
 // 轮播图以及旁边新闻列表
 function newsListInfo() {
     $.get('/news_list',(headlineNews)=> {
@@ -107,22 +79,20 @@ function newsListInfo() {
         }
     });
 }
-
-
 // 主要的新闻部分
 function displaynews(index) {
     $('#simplecontent').html('');
-    $.get(`/all_news_pading?page=${index}`,(nesws)=>{
-        for(i=0;i<nesws.length;i++){
+    $.get(`/all_news_pading?page=${index}`,(news)=>{
+        for(i=0;i<news.length;i++){
             let li = $('<li></li>');
             let img = $('<img>');
             let h3 = $('<h3></h3>');
             let p = $('<p></p>');
             let a = $('<a></a>');
-            img.attr('src',`${nesws[i].news_image}`);
-            a.attr('href',`http://localhost:3000/HTML/detail.html?id=${nesws[i].id}`);
-            h3.html(`${nesws[i].news_title}`);
-            p.html(`${nesws[i].news_content}`);
+            img.attr('src',`${news[i].news_image}`);
+            a.attr('href',`http://localhost:3000/HTML/detail.html?id=${news[i].id}`);
+            h3.html(`${news[i].news_title}`);
+            p.html(`${news[i].news_content}`);
             li.append(img);
             li.append(h3);
             li.append(p);
