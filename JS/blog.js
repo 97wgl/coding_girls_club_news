0794e4501214d7e9dc6789146e179f6c88b0a62b
Christine233 <1040551136@qq.com>
2017-08-16 15:55:11 +0800

    ,,,

--- JS/blog.js
+++ JS/blog.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
 
+    let count;
+    $.ajax({
+        type:'Get',
+        url:'all_blog_count',
+        async:false,
+        success:function (data) {
+            count = data[0].all_blogs_count;
+        }
+    });
 
 });
